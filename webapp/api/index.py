917542743283aa469cc79970a0f6f16cd8ce2c10
YashwantBhosale <yashwantbhosale07@gmail.com>
2025-01-14 00:44:25 +0530

    test: tried fixing python server proxy

--- webapp/api/index.py
+++ webapp/api/index.py
@@ -84,4 +84,4 @@ def directPredict():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone', // For Vercel deployment
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*', // Proxy to serverless function for API
-      },
-    ];
-  },
+	reactStrictMode: true,
+	output: "standalone", // For Vercel deployment
+	async rewrites() {
+		console.log("VERCEL_ENV:", process.env.VERCEL_ENV); // This will help debug the issue
+		return [
+			{
+				source: "/api/:path*",
+				destination:
+					process.env.VERCEL_ENV === "production"
+						? "https://hand-written-digit-recognition-delta.vercel.app/api/:path*"
+						: "http://localhost:5328/api/:path*",
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
