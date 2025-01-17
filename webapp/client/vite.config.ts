e135c97902aa3599d55930c3dd3aa9772dd30de1
YashwantBhosale <yashwantbhosale07@gmail.com>
2025-01-17 14:25:54 +0530

    update: configured frontend for deployment

--- webapp/client/vite.config.ts
+++ webapp/client/vite.config.ts
@@ -3,20 +3,20 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000/api",
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
+  server: {
+    proxy: {
+      "/api": {
+        target: `https://hand-written-digit-recognition-ten.vercel.app/api`,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
-			},
-		},
-	},
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+      },
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
